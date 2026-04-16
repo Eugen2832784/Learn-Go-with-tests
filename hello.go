d4a900d378469eb8cec469882b403c5815874bd8
EugeneDev <kovalonokjevgenij@gmail.com>
2026-04-16 08:13:53 +0300

    add some languages,switch statement

--- hello.go
+++ hello.go
@@ -1,12 +1,30 @@
 package main
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const spanish = "Spanish"
+const frenchHelloPrefix = "Bonjour, "
+const french = "French"
+const russianHelloPrefix = "Привет, "
+const russian = "Russian"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case russian:
+		prefix = russianHelloPrefix
+	}
+
+	return prefix + name
 }
 
 func main() {
